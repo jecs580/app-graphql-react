6e4ff82671b516679c4b07134ed017e84fd481bf
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-02-28 21:57:51 -0400

    Creacion de componentes a utilizar

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const client = new ApolloClient({
+  uri:'http://localhost:4000'
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
